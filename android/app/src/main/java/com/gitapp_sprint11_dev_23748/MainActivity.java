3129fe99b8da49d8b14645d9b6a566ae5862acca
Crowdbotics <engineering@crowdbotics.com>
2022-03-28 05:37:26 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gitapp_sprint11_dev_23748/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gitapp_sprint11_dev_23748;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "gitapp_sprint11_dev_23748";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "gitapp_sprint11_dev_23748",
+  url: "https://gitapp_sprint11_dev_23748.botics.co",
+  api: "https://gitapp_sprint11_dev_23748.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
